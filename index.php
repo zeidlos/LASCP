50a4fd9850db44ede24ce44d55567201fab5758a
root <root@marsoc-yoma.(none)>
2011-02-18 22:29:25 +0100

    Interface stylingw

--- index.php
+++ index.php
@@ -5,13 +5,14 @@ $running=0;
 
 <html>
 <head><title>MARSOC Testserver - Mission Upload Tool</title>
-
-
+<link href="./css/style.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
 <div id="wrapper">
-<h1>Welcome to the MARSOC Testserver Mission Upload Tool</h1>
+<img src="http://dl.dropbox.com/u/2854558/MARSOC_MUT.png" width="500px" />
+<div id="content">
 <?php $running=check_pid();
 
 if($running==1)
@@ -20,8 +21,9 @@ if($running==1)
     echo('<span class="danger"><a href="index.php?action=stop">STOP THE SERVER!</a></span>');
   } else {
     echo('The server is not running. Now you can <br />');
-    echo('<a href="index.php?action=upload">Upload a Mission</a><br />');
-    echo('<a href="index.php?action=start">Start the server</a>');
+    echo('<a href="index.php?action=upload"><span class="button">Upload a Mission</span></a><br />');
+    echo('<a href="index.php?action=start"><span class="button">Start the server</span></a>');
   }
 ?>
+</div>
 </div>

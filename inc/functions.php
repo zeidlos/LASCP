cb98d59842e6f6bbf01f3e4febdea93adcb95c5e
banshee49th <banshee@49thblackdiamonds.com>
2011-02-24 19:33:16 +0100

    inc/config.php values added

--- inc/functions.php
+++ inc/functions.php
@@ -2,31 +2,31 @@
 
 function check_pid() 
 {
-  $pid_file = '/var/www/admin/inc/server.pid';
+  $pid_file = $server_path.'inc/server.pid';
   if (file_exists($pid_file))
   {
-    $pid = shell_exec("cat /var/www/admin/inc/server.pid");
+    $pid = shell_exec('cat '.$server_path.'inc/server.pid');
     return 1;
   }
 }
 
 function kill_server()
 {
-  $pid_file = '/var/www/admin/inc/server.pid';
+  $pid_file = $server_path.'inc/server.pid';
   if (file_exists($pid_file)) 
   {
-    $pid = shell_exec("cat /var/www/admin/inc/server.pid"); 
-    shell_exec("/usr/bin/sudo /bin/kill $pid");
-    shell_exec("/usr/bin/sudo rm /var/www/admin/inc/server.pid");
+    $pid = shell_exec('cat '.$server_path.'inc/server.pid'); 
+    shell_exec("/usr/bin/sudo -u marsoc /bin/kill $pid");
+    shell_exec('/usr/bin/sudo -u marsoc rm '.$server_path.'inc/server.pid');
   }
 }
 
 function start_server()
 {
-  $pid_file = shell_exec("/var/www/admin/inc/server.pid");
+  $pid_file = shell_exec($server_path.'inc/server.pid');
   if (!file_exists($pid_file))
   {
-    shell_exec("/usr/bin/sudo /var/www/admin/inc/run_server.sh");
+    shell_exec('/usr/bin/sudo -u marsoc '.$server_path.'inc/run_server.sh');
   }
  }
 

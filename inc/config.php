5a9f57abbeae11b89bbe93fb582a51165a84512a
banshee49th <banshee@49thblackdiamonds.com>
2011-02-27 06:49:20 +0100

    v0.2. Added update functionality, codecleaning, documentation

---
+++ inc/config.php
@@ -0,0 +1,33 @@
+<?php
+/*
+MARSOC Server Control Pannel
+Version: 0.2
+Date: 2011-02-27
+Author: Banshee
+URL: http://going4.com
+
+MARSOC Server Control Pannel is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. Permissions beyond the scope of this license may be available at http://going4.com.
+
+http://creativecommons.org/licenses/by-nc-sa/3.0/
+*/
+
+
+// URL of Header graphic. Static or relative.
+$header_img = 'http://dl.dropbox.com/u/2854558/MSCP.png';
+
+// Servername. Will get displayed on the interface.
+// It won't be used in for the serverconfig.
+$server_name = 'MARSOC ACE Public';
+
+// Sudo User. User wich the server will own the server process
+$sudo_user = 'marsoc';
+
+// Webdir path
+// Path where this tool resides in
+$server_path ='/var/www/admin/';
+
+// ArmA directory path
+// Path where ArmA files reside in
+$arma_dir ='/data/a2oa/';
+
+?>

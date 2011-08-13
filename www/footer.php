40fd41df2bed71839b09fd35fa0a0428bee2cf35
Banshee <banshee@tier1ops.eu>
2011-08-13 18:28:16 +0200

    Removed unnessesary php and added TODO marker for auto version detection.

--- www/footer.php
+++ www/footer.php
@@ -1,9 +1,6 @@
-<?php
-
-?>
-
 <div id="footer">
 <a href="http://dev-heaven.net/projects/lascp">LASCP</a> v0.3 brought to you by <a href="http://tier1ops.eu">Tier1</a>
+<!-- TODO Add autodetection for Version -->
 </div>
 </div>
 </body>

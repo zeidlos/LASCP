962836574623036794fe8ad8ccc1a2b99e5c6416
Banshee <banshee@tier1ops.eu>
2011-08-13 23:39:56 +0200

    Changed sudo user to $sudo_user from the config. Now everything _should_ work. :)

--- www/inc/functions.php
+++ www/inc/functions.php
@@ -1,13 +1,13 @@
 <?php
 include($server_path.'inc/config.php');
 /*
-MARSOC Server Control Pannel
+Linux Arm Server Control Pannel
 Version: 0.2
 Date: 2011-02-27
 Author: Banshee
-URL: http://going4.com
+URL: http://tier1ops.eu
 
-MARSOC Server Control Pannel is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. Permissions beyond the scope of this license may be available at http://going4.com.
+Linux Arma Server Control Pannel is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. Permissions beyond the scope of this license may be available at http://going4.com.
 
 http://creativecommons.org/licenses/by-nc-sa/3.0/
 */
@@ -20,14 +20,14 @@ function server_status()
 
 function stop_server($server_path)
 {
-  shell_exec('/usr/bin/sudo -u marsoc '.$server_path.'/inc/arma2oaserver stop');
-//  shell_exec('/usr/bin/sudo -u marsoc '.$server_path.'/inc/testserver stop');
+  shell_exec('/usr/bin/sudo -u '.$sudo_user.' '.$server_path.'/inc/arma2oaserver stop');
+//  shell_exec('/usr/bin/sudo -u '.$sudo_user.' '.$server_path.'/inc/testserver stop');
 }
 
 function start_server($server_path)
 {
-    shell_exec('/usr/bin/sudo -u marsoc '.$server_path.'inc/arma2oaserver start');
-//    shell_exec('/usr/bin/sudo -u marsoc '.$server_path.'inc/testserver start');
+    shell_exec('/usr/bin/sudo -u '.$sudo_user.' '.$server_path.'inc/arma2oaserver start');
+//    shell_exec('/usr/bin/sudo -u '.$sudo_user.' '.$server_path.'inc/testserver start');
  }
 
 ?>

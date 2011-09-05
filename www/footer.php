15d3994a1744b51aa5087ff45685fdde79e5bb59
Banshee <banshee@tier1ops.eu>
2011-09-05 19:00:26 +0200

    Switched from fixed value to the value from config for lascp_version

--- www/footer.php
+++ www/footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-<a href="http://dev-heaven.net/projects/lascp">LASCP</a> v0.3 brought to you by <a href="http://tier1ops.eu">Tier1</a>
+<a href="http://dev-heaven.net/projects/lascp">LASCP</a> <?php echo($lascp_version);?> brought to you by <a href="http://tier1ops.eu">Tier1</a>
 <!-- TODO Add autodetection for Version -->
 </div>
 </div>
